0519ea3e8a7d6e61ce9d1ed8e21ea5168a363174
Aaron Loucks <aloucks@cofront.net>
2019-04-06 16:25:57 -0400

    Adding test shaders compilation helper script

---
+++ tests/shaders/compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+DIR=$(dirname ${BASH_SOURCE[0]})
+FILES=$(find $DIR -name "*.glsl")
+
+for FILE in ${FILES}; do
+    NAME_GLSL=$(basename ${FILE})
+    NAME_SPV=${NAME_GLSL/\.glsl/\.spv}
+    STAGE="unknown"
+    case $NAME_SPV in
+        *".vert.spv")
+            STAGE="vert";;
+        *".frag.spv")
+            STAGE="frag";;
+        *".comp.spv")
+            STAGE="comp";;
+    esac
+    PATH_SPV=${DIR}/${NAME_SPV}
+    GLSL_TIME=$(stat -c %Y ${FILE})
+    if [[ ! -f ${PATH_SPV} || ${GLSL_TIME} > $(stat -c %Y ${PATH_SPV}) ]]; then
+        echo "Compiling: ${FILE}"
+        glslc -fshader-stage=${STAGE} ${FILE} -o ${PATH_SPV}
+    fi
+done
