d968abc57966c25ca2bcb1841330191540604e0d
Aaron Loucks <aloucks@cofront.net>
2019-04-19 08:49:44 -0400

    Use uniform/attirbute/varying variable prefixes in example shaders

--- examples/shaders/triangle.frag.glsl
+++ examples/shaders/triangle.frag.glsl
@@ -1,9 +1,9 @@
 #version 450
 
-layout(location = 0) in vec3 inColor;
+layout(location = 0) in vec3 v_Color;
 
 layout(location = 0) out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(inColor, 1.0);
+    fragColor = vec4(v_Color, 1.0);
 }
--- examples/shaders/triangle.vert.glsl
+++ examples/shaders/triangle.vert.glsl
@@ -1,16 +1,16 @@
 #version 450
 
-layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec3 inColor;
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec3 a_Color;
 
-layout(location = 0) out vec3 outColor;
+layout(location = 0) out vec3 v_Color;
 
 layout(set = 0, binding = 0) uniform Uniforms {
-    mat4 clip;
-    float time;
+    mat4 u_Clip;
+    float u_Time;
 };
 
 void main() {
-    outColor = (((cos(time) + 1.0) / 3.0) + 0.25) * inColor;
-    gl_Position = clip * vec4(inPosition, 1.0);
+    v_Color = (((cos(u_Time) + 1.0) / 3.0) + 0.25) * a_Color;
+    gl_Position = u_Clip * vec4(a_Position, 1.0);
 }
